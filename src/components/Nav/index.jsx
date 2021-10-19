fbdb31c26ffc8ad3bcf69e2da76866c7fad9e316
ngoctuyen <nntuyen1505@gmail.com>
2021-10-19 10:28:06 +0700

    add file

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function nav() {
+    return (
+        <nav className="nav">
+        <ul>
+          <li className="li_login">
+            <a href="#">Đăng nhập</a>
+            <a href="#">Đăng ký</a>
+          </li>
+          <li className="active">
+            <a href="#">Trang chủ</a>
+          </li>
+          <li>
+            <a href="#">CFD Team</a>
+          </li>
+          <li>
+            <a href="#">Khóa Học</a>
+          </li>
+          <li>
+            <a href="#">Dự Án</a>
+          </li>
+          <li>
+            <a href="#">Liên hệ</a>
+          </li>
+        </ul>
+      </nav>
+    )
+}
